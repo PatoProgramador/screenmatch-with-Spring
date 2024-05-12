0662531257913e2ac8ff1efbd5595fc69bd05e52
PatoProgramador <skpates18@gmail.com>
2024-05-12 18:35:38 -0500

    Add Episodes record and url

--- src/main/java/com/patolearn/screenmatch/ScreenmatchApplication.java
+++ src/main/java/com/patolearn/screenmatch/ScreenmatchApplication.java
@@ -1,5 +1,6 @@
 package com.patolearn.screenmatch;
 
+import com.patolearn.screenmatch.model.DatosEpisodio;
 import com.patolearn.screenmatch.model.DatosSerie;
 import com.patolearn.screenmatch.service.ConsumoAPI;
 import com.patolearn.screenmatch.service.Conversor;
@@ -18,10 +19,14 @@ public class ScreenmatchApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		ConsumoAPI consumoAPI = new ConsumoAPI();
 		Conversor conversor = new Conversor();
-
+		//Se obtiene la serie
 		String json = consumoAPI.obtenerDatos("https://www.omdbapi.com/?t=game+of+thrones&apikey=7b4848af");
 		System.out.println(json);
 		DatosSerie datos = conversor.obtenerDatos(json, DatosSerie.class);
 		System.out.println(datos);
+		// Se obtiene el primer episodio de la primer season
+		json = consumoAPI.obtenerDatos("https://www.omdbapi.com/?t=game+of+thrones&Season=1&episode=1&apikey=7b4848af");
+		DatosEpisodio episodio = conversor.obtenerDatos(json, DatosEpisodio.class);
+		System.out.println(episodio);
 	}
 }
