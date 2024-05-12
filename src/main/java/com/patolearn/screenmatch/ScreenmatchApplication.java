0fa07c37cc4fa871498d9721bbb7692a2ca21dff
PatoProgramador <skpates18@gmail.com>
2024-05-12 18:47:01 -0500

    Add Seasons list, record andprint

--- src/main/java/com/patolearn/screenmatch/ScreenmatchApplication.java
+++ src/main/java/com/patolearn/screenmatch/ScreenmatchApplication.java
@@ -2,11 +2,15 @@ package com.patolearn.screenmatch;
 
 import com.patolearn.screenmatch.model.DatosEpisodio;
 import com.patolearn.screenmatch.model.DatosSerie;
+import com.patolearn.screenmatch.model.DatosTemporada;
 import com.patolearn.screenmatch.service.ConsumoAPI;
 import com.patolearn.screenmatch.service.Conversor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
@@ -28,5 +32,14 @@ public class ScreenmatchApplication implements CommandLineRunner {
 		json = consumoAPI.obtenerDatos("https://www.omdbapi.com/?t=game+of+thrones&Season=1&episode=1&apikey=7b4848af");
 		DatosEpisodio episodio = conversor.obtenerDatos(json, DatosEpisodio.class);
 		System.out.println(episodio);
+		// Se obtiene la primera temporada
+		List<DatosTemporada> temporadas = new ArrayList<>();
+		// se añade cada temporada a la lista
+		for (int i = 1; i <= datos.totalTemporadas(); i++) {
+			json = consumoAPI.obtenerDatos("https://www.omdbapi.com/?t=game+of+thrones&Season=" + i +"&apikey=7b4848af");
+			DatosTemporada datosTemporada = conversor.obtenerDatos(json, DatosTemporada.class);
+			temporadas.add(datosTemporada);
+		}
+		temporadas.forEach(System.out::println);
 	}
 }
