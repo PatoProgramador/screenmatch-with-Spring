abe931909b0cc04b19ee7ba08f53d58a7e124288
PatoProgramador <skpates18@gmail.com>
2024-05-12 20:20:51 -0500

    Add .env config

--- src/main/java/com/patolearn/screenmatch/main/Main.java
+++ src/main/java/com/patolearn/screenmatch/main/Main.java
@@ -4,6 +4,8 @@ import com.patolearn.screenmatch.model.DatosSerie;
 import com.patolearn.screenmatch.model.DatosTemporada;
 import com.patolearn.screenmatch.service.ConsumoAPI;
 import com.patolearn.screenmatch.service.Conversor;
+import io.github.cdimascio.dotenv.Dotenv;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -11,16 +13,19 @@ import java.util.Scanner;
 public class Main {
     private final Scanner SCANNER = new Scanner(System.in);
     private ConsumoAPI consumoAPI = new ConsumoAPI();
+    private Dotenv dotenv = Dotenv.load();
     private final String URL_BASE = "https://www.omdbapi.com/?t=";
-    private final String API_KEY = "&apikey=7b4848af";
+    private final String API_KEY = "&apikey=" + dotenv.get("API_KEY");
     private Conversor conversor = new Conversor();
 
     public void mostrarMenu() {
+        System.out.println(API_KEY);
         System.out.println("Por favor, escribe el nombre de la serie que deseas buscar: ");
         // Buscar datos generales de la serie
         String nombreSerie = SCANNER.nextLine();
         String json = consumoAPI.obtenerDatos(URL_BASE + nombreSerie.replace(" ", "+") + API_KEY);
         DatosSerie datos = conversor.obtenerDatos(json, DatosSerie.class);
+        System.out.println(datos);
         // Busca los datos de todas las temporadas
         List<DatosTemporada> temporadas = new ArrayList<>();
         // se añade cada temporada a la lista
