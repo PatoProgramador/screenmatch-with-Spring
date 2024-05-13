a9793972c7a1fea8afde403b48ca1a3669cd6179
PatoProgramador <skpates18@gmail.com>
2024-05-12 20:38:06 -0500

    Lambda function

--- src/main/java/com/patolearn/screenmatch/main/Main.java
+++ src/main/java/com/patolearn/screenmatch/main/Main.java
@@ -1,5 +1,6 @@
 package com.patolearn.screenmatch.main;
 
+import com.patolearn.screenmatch.model.DatosEpisodio;
 import com.patolearn.screenmatch.model.DatosSerie;
 import com.patolearn.screenmatch.model.DatosTemporada;
 import com.patolearn.screenmatch.service.ConsumoAPI;
@@ -13,13 +14,12 @@ import java.util.Scanner;
 public class Main {
     private final Scanner SCANNER = new Scanner(System.in);
     private ConsumoAPI consumoAPI = new ConsumoAPI();
-    private Dotenv dotenv = Dotenv.load();
+    private final Dotenv DOTENV = Dotenv.load();
     private final String URL_BASE = "https://www.omdbapi.com/?t=";
-    private final String API_KEY = "&apikey=" + dotenv.get("API_KEY");
+    private final String API_KEY = "&apikey=" + DOTENV.get("API_KEY");
     private Conversor conversor = new Conversor();
 
     public void mostrarMenu() {
-        System.out.println(API_KEY);
         System.out.println("Por favor, escribe el nombre de la serie que deseas buscar: ");
         // Buscar datos generales de la serie
         String nombreSerie = SCANNER.nextLine();
@@ -34,6 +34,16 @@ public class Main {
             DatosTemporada datosTemporada = conversor.obtenerDatos(json, DatosTemporada.class);
             temporadas.add(datosTemporada);
         }
-        temporadas.forEach(System.out::println);
+//        temporadas.forEach(System.out::println);
+
+        // Mostrar solo el titulo de los episodios para las temporadas
+//        for (int i = 0; i < datos.totalTemporadas(); i++) {
+//            List<DatosEpisodio> episodiosTemporada = temporadas.get(i).episodios();
+//            for (int j = 0; j < episodiosTemporada.size(); j++) {
+//                System.out.println(episodiosTemporada.get(j).titulo());
+//            }
+//        }
+        // uso de funcion lambda
+        temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
     }
 }
